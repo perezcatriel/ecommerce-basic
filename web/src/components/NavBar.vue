ea4affbc00423669bfe1938044667c01ab05816c
Catriel <perezcatriel@gmail.com>
2023-08-13 18:46:28 -0300

    icons

--- web/src/components/NavBar.vue
+++ web/src/components/NavBar.vue
@@ -1,6 +1,9 @@
 <template>
-  <div>
-    barra de navegacion
+  <div class="navbar">
+    <img src="../assets/icons/menu.svg" alt="">
+    <h1>Mar de Cores</h1>
+    <img src="../assets/icons/search.svg" alt="">
+    <img src="../assets/icons/user.png" alt="">
   </div>
 </template>
 
@@ -10,4 +13,13 @@ export default {
 };
 </script>
 
-<style scoped></style>
+<style scoped>
+.navbar {
+  display: flex;
+  justify-content: space-around;
+}
+
+h1 {
+  font-family: var(--font-cursive);
+}
+</style>
