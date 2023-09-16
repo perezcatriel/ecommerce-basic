76a9e7c434c42f39a1f2b79ef6f99be0e293f34c
perezcatriel <perezcatriel@gmail.com>
2023-09-16 10:15:28 -0300

    Whatsapp notificated

--- web/src/components/WhatsApp.vue
+++ web/src/components/WhatsApp.vue
@@ -1,5 +1,10 @@
 <template>
-  <a href="https://wa.me/543541204400"><img src="../assets/icons/whatsapp.png" alt="WhatsApp"></a>
+  <div class="whatsapp-container">
+    <a href="https://wa.me/543541204400">
+      <img src="../assets/icons/whatsapp.png" alt="WhatsApp">
+      <div v-if="showNotification" class="notification">1</div>
+    </a>
+  </div>
 </template>
 
 <script>
@@ -7,17 +12,53 @@ export default {
   data() {
     return {
       name: 'WhatsApp',
+      showNotification: false,
     };
+  },
+  mounted() {
+    setTimeout(() => {
+      this.showNotification = true;
+    }, 5000);
   },
 };
 </script>
 
 <style scoped>
+.whatsapp-container {
+  position: relative;
+}
+
 img {
   width: 40px;
   height: 40px;
   position: fixed;
   bottom: 20px;
   right: 20px;
+}
+
+.notification {
+  position: fixed;
+  top: calc(100vh - 85px);
+  right: 15px;
+  background-color: var(--arena-dorada);
+  color: var(--font-dark);
+  border-radius: 50%;
+  box-shadow: 0px 0px 5px var(--font-dark);
+  width: 20px;
+  height: 20px;
+  text-align: center;
+  line-height: 20px;
+  font-size: 12px;
+  animation: fadeIn 1s;
+  z-index: 20;
+}
+
+@keyframes fadeIn {
+  from {
+    opacity: 0;
+  }
+  to {
+    opacity: 1;
+  }
 }
 </style>
